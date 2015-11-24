89eea244be7f98cf375a643f78277256d707528f
Thomas Ebel <conseal@gmx.net>
2015-11-24 09:54:33 +0100

    modified Compare_names

--- Compare_names_of_datasets.R
+++ Compare_names_of_datasets.R
@@ -2,11 +2,11 @@ setwd("H:\\Dateneingangskontrolle\\")
 
 library(foreign)
 
-mitg <- read.spss("datei1.sav", to.data.frame = TRUE)
-mitgChor <- read.spss("datei2.sav", to.data.frame = TRUE)
+file1 <- read.spss("file1.sav", to.data.frame = TRUE)
+file2 <- read.spss("file2.sav", to.data.frame = TRUE)
 
-a1 <- names(mitg)
-a2 <- names(mitgChor)
+a1 <- names(file1)
+a2 <- names(file2)
 which(!a1 %in% a2) 
 a1[which(!a1 %in% a2)]
 which(!a2 %in% a1) 
